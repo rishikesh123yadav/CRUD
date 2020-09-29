fff8d255a87e69776711d3de5dd06bd659a7b05f
Rishikesh <rishikesh@codemymobile.com>
2020-09-29 12:22:01 +0530

    setting up project

---
+++ CRUD/serializers.py
@@ -0,0 +1,8 @@
+#to create api
+from rest_framework import serializers
+from .models import Post
+      
+class PostSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Post
+    fields = ('id', 'title', 'message')
